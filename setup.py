0964c4550b2916b80b26c2cf5fe92cff2940203a
Jian <jianchf@outlook.com>
2025-08-16 12:10:55 -0400

    removed HF version limit
    -removed HF Transformers limit <=4.47.1
    
    -updated test.yml

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             "faster-whisper"
         ],
         "hf": [
-            "transformers>=4.23.0,<=4.47.1",
+            "transformers>=4.23.0",
             "optimum",
             "accelerate"
         ],
